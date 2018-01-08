25ece6b65aa82cab2eb76cf35b24acafbe2551da
Daniel Krusch <daniel.krusch@gmail.com>
2018-01-08 15:34:51 -0500

    Initial commit

---
+++ 005_my_game/app/src/main/java/com/example/backup/my_game/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.backup.my_game;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.example.backup.my_game.view.ReflexView;
+
+public class MainActivity extends AppCompatActivity {
+    private ReflexView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout layout = findViewById(R.id.relativeLayout);
+        gameView = new ReflexView(this,getPreferences(Context.MODE_PRIVATE), layout);
+
+        layout.addView(gameView, 0);
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume(this);
+    }
+}
